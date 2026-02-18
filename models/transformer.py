41abd5068377ba551f6f3d00492857d57a887b41
Youssef Elkilany <youssof.kilaney@gmail.com>
2026-02-18 14:48:11 +0200

    dbg

--- models/transformer.py
+++ models/transformer.py
@@ -98,6 +98,10 @@ class PositionalEncoding(nn.Module):
         Args:
             x: (seq_len, batch, d_model)
         """
+        print(f"{x.shape = }")
+        print(f"{self.pe.shape = }")
+        print(f"{self.pe[: x.shape[0], :] = }")
+
         x = x + self.pe[: x.shape[0], :]
         return self.dropout(x)
 
